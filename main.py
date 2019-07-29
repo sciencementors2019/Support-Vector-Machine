3e4cd36e0c39bf561499493a1161c644ca5baadd
lockness-Ko <videolk9373@gmail.com>
2019-07-29 20:29:51 +1000

    minor change (experimenting)

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from brianthebrain import *
 
 svm = brian('scale','rbf',1.0)
 
-X = [[0, 0], [1, 1]]
+X = [[2, 2], [1, 1]]
 y = [0, 1]
 svm.load(X,y)
 svm.train()
-print(svm.predict([[2., 2.]]))
+print(svm.predict([[0, 0]]))
