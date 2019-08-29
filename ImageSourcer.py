8a56d05e4b9cafd5471621254be609a74e1c712d
Whadrah <50134731+Whadrah@users.noreply.github.com>
2019-08-29 21:06:56 +1000

    Changed excel formatting

--- ImageSourcer.py
+++ ImageSourcer.py
@@ -13,31 +13,47 @@ os.chdir(Directory)
 
 #Collects the data from the image library, and utilises the image processor
 folders, labels, images, imFeatures = ['triangle', 'star', 'square', 'circle'], [], [], []
+ID = 1
 for folder in folders:
     print(folder)
     for path in os.listdir(os.getcwd() +'/'+folder):
         imageFeature = imageProcessor(folder+'/'+path)
         img = imageFeature.img
-        images.append(img)
+        images.append("ID" + str(ID))
         labels.append(folder)
         imFeatures.append(imageFeature.detail())
+        ID +=1
 
-#Sorts the data into a Train/Test ratio of 1:5 / 20%:80%
-toTrain = 0
-trainLabels, testLabels = [],[]
-trainImages, testImages = [], []
+
+trainLabels = []
+trainImages = []
 
 for image, label, features in zip(images, labels, imFeatures):
-    if toTrain < 5:
+    DFCSet = []
+    PerimeterSet = []
+    for i in range(16):
+            Perimeter = features["Perimeter"]
+            try:
+                PerimeterSet.append(Perimeter[i])
+            except:
+                PerimeterSet.append(0)
         #Appends the image data right next to the image itself to for the X value
-        trainImages.append([image, features["Vertices"], features["Perimeter"], features["SumOfAngles"], features["DistFromCentre"]] )
-        trainLabels.append(label)
-        toTrain += 1
-    else:
-        testImages.append([image, features["Vertices"], features["Perimeter"], features["SumOfAngles"], features["DistFromCentre"]])
-        testLabels.append(label)
+    for i in range(16):
+        DFC = features["DistFromCentre"]
+        try:
+            DFCSet.append(DFC[i])
+        except IndexError:
+            DFCSet.append(0)
+            continue
+    InsertList = []
+    InsertList.append(image)
+    InsertList.append(features["Vertices"])
+    InsertList.extend(PerimeterSet)
+    InsertList.append(features["SumOfAngles"])
+    InsertList.extend(DFCSet)
+    trainImages.append(InsertList)                    
+    trainLabels.append(label)
     
-        toTrain = 0
 
 #trainImages = np.array(trainImages, dtype = object)
 #testImages = np.array(testImages, dtype = object)
@@ -54,31 +70,14 @@ trainImagesSheet.title = "trainImagesSheet"
 rowNumber = 1
 
 for trainImage in trainImages:
-    
-    trainImagesSheet.cell(row = rowNumber, column = 1).value = (np.array2string(trainImage[0]))
-    trainImagesSheet.cell(row = rowNumber, column = 2).value = str(trainImage[1])
-    trainImagesSheet.cell(row = rowNumber, column = 3).value = str(trainImage[2])
-    trainImagesSheet.cell(row = rowNumber, column = 4).value = str(trainImage[3])
-    trainImagesSheet.cell(row = rowNumber, column = 5).value = str(trainImage[4])
-
-    rowNumber +=1
-
-testImagesSheet = dataModels.active
-testImagesSheet = dataModels.create_sheet("xd")
-testImagesSheet.title = "testImageSheet"
-
-rowNumber = 1
-
-for testImage in testImages:
-    testImagesSheet.cell(row = rowNumber, column = 1).value = (np.array2string(trainImage[0]))
-    testImagesSheet.cell(row = rowNumber, column = 2).value = str(testImage[1])
-    testImagesSheet.cell(row = rowNumber, column = 3).value = str(testImage[2])
-    testImagesSheet.cell(row = rowNumber, column = 4).value = str(testImage[3])
-    testImagesSheet.cell(row = rowNumber, column = 5).value = str(testImage[4])
-
+    columnNum = 1
+    for i in range(len(trainImage)):
+        trainImagesSheet.cell(row = rowNumber, column = columnNum).value = (trainImage[i])
+        columnNum += 1
     rowNumber +=1
 
 rowNumber = 1
+
 trainLabelsSheet = dataModels.active
 trainLabelsSheet = dataModels.create_sheet("xd")
 trainLabelsSheet.title = "trainLabelsSheet"
@@ -87,14 +86,6 @@ for trainLabel in trainLabels:
 
     rowNumber += 1
     
-testLabelsSheet = dataModels.active
-testLabelsSheet = dataModels.create_sheet("xd")
-testLabelsSheet.title = "testLabelsSheet"
-rowNumber = 1
-
-for testLabel in testLabels:
-    testLabelsSheet.cell(row = rowNumber, column = 1).value = testLabel
-    rowNumber += 1
 
 dataModels.save(filename = 'C:/Users/hamis/Documents/SVMModels/dataModels.xlsx')
 
