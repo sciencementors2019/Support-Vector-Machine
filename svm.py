d2b7b924b200f412ac88ae4a58988cfaa64a0cc6
lockness-Ko <videolk9373@gmail.com>
2019-09-03 15:43:42 +1000

    so guys we did is we fixed the key error bug

--- svm.py
+++ svm.py
@@ -4,7 +4,7 @@ from scipy import stats
 from scipy import linalg
 import pylab as pl
 from sklearn import svm, linear_model
-from sklearn.model_selection import cross_validate
+from sklearn.model_selection import train_test_split
 import timeit
 from dataread import *
 
@@ -13,27 +13,37 @@ from dataread import *
 
 
 n_samples = 14970 #this is the number of images in our dataset
-n_features = 35 #num of features per feature vector
+n_features = 34 #num of features per feature vector
 data = np.empty((n_samples, n_features), dtype=np.int) #creates a 2d array with the first dimension number of samples and second number of features thus the matrix is 502 x 10 
 target = np.empty((n_samples), dtype=np.int) #creates a 1d array that is of length sample number ie 502
 qid = np.empty((n_samples), dtype=np.int)
 
 trainDat = trainData()
 trainLabel = trainLabels()
-print(trainDat)
-for y in range(n_samples):
-    for x in range(n_features):
-        print(trainDat[x][y])
-        data[y][x] = trainDat[y][x]
+# print(trainDat)
+for y in range(n_samples-1):
+    for x in range(1,n_features):
+        data[y][x] = trainDat[x][y]# X axis is first index and Y is second!!!!! why numpy...
 
-print(data)
+for i in range(n_samples-1):
+    if trainLabel[0][i] == 'triangle':# X axis is first index and Y is second!!!!! why numpy...
+        target[i] = 0  
+    if trainLabel[0][i] == 'circle':# X axis is first index and Y is second!!!!! why numpy...
+        target[i] = 0  
+    if trainLabel[0][i] == 'square':# X axis is first index and Y is second!!!!! why numpy...
+        target[i] = 0  
+    if trainLabel[0][i] == 'star':# X axis is first index and Y is second!!!!! why numpy...
+        target[i] = 0       
+     
+print(target)
 
+X_train, X_test, y_train, y_test = train_test_split(data, target, train_size = 0.8)
 # split into train and test set
-cv = cross_validate.StratifiedShuffleSplit(target, test_size=.8) #TODO
-train, test = iter(cv).next() #creates a test mask array and a train mask array. these arrays are the indexes of x and y that are assigned to each set
+# cv = cross_validate.StratifiedShuffleSplit(target, test_size=.8) #TODO
+# train, test = iter(cv).next() #creates a test mask array and a train mask array. these arrays are the indexes of x and y that are assigned to each set
 
-X_train, y_train = data[train], target[train] #using the train index mask create an copy array of both x and y respectivly that is only filled with the training items
-X_test, y_test, qid_test = data[test], target[test], qid[test] #using the test index mask create an copy array of both x and y respectivly that is only filled with the test items
+# X_train, y_train = data[train], target[train] #using the train index mask create an copy array of both x and y respectivly that is only filled with the training items
+# X_test, y_test, qid_test = data[test], target[test], qid[test] #using the test index mask create an copy array of both x and y respectivly that is only filled with the test items
 
 print("SVM starting")		
 #SVM        
